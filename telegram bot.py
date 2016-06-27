0bd6a5fbd0bebffdd73b608bb596c80a979519e1
hamed <ham.jam1375@gmail.com>
2016-06-28 01:08:09 +0430

    true token

--- telegram bot.py
+++ telegram bot.py
@@ -3,10 +3,10 @@ from telegram import message, sticker, emoji, bot
 
 
 def main():
-    updater = Updater(token='179409747:AAFMOpkXpZLQcqjVVlzvIl4FidpFJ1z75I0')
+    updater = Updater(token='205572739:AAEFs_y41exvonAdtj_ZHV9koxdzRrd7nz4')
 
     dispatcher = updater.dispatcher
-    myBot = bot.Bot(token='179409747:AAFMOpkXpZLQcqjVVlzvIl4FidpFJ1z75I0')
+    myBot = bot.Bot(token='205572739:AAEFs_y41exvonAdtj_ZHV9koxdzRrd7nz4')
     while True:
         myBot.sendMessage(updtext='Hi')
     updater.start_polling()
